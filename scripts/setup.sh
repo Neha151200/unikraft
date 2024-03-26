e04743570ddb8fd9501d3bbc7aae45d2706abb5e
SDNU2024 <sdnuproject@gmail.com>
2024-03-26 11:15:10 +0530

    Update README.md

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 test ! -d "workdir" && echo "Cloning unikraft..." || true
-test ! -d "workdir/unikraft" && git clone https://github.com/unikraft/unikraft workdir/unikraft || true
+test ! -d "workdir/unikraft" && git clone https://github.com/Neha151200/unikraft.git workdir/unikraft || true
+#test ! -d "workdir/unikraft" && git clone https://github.com/unikraft/unikraft workdir/unikraft || true
